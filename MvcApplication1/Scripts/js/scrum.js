ccdedf50c9a16e5b2238aac15e99e59c0510d8df
Josh Pierro <josh.pierro@gmail.com>
2013-12-04 23:02:55 -0700

    stub out UI

---
+++ MvcApplication1/Scripts/js/scrum.js
@@ -0,0 +1,22 @@
+﻿
+function teamCtrl($scope) {
+
+    $scope.members = [];
+    
+    $scope.addTeamMember = function () {
+        
+        $scope.members.push({
+            memberName: $scope.scrum.team.memberName,
+            memberEmail: $scope.scrum.team.memberCapacity
+        });
+        
+        $scope.scrum.team.memberName = '';
+        $scope.scrum.team.memberCapacity = '';
+    
+    };
+
+   
+
+}
+
+
