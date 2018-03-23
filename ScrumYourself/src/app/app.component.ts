970a287e415c38d4128b1cff37533b7988865376
Josh Pierro <josh_pierro@fsafood.com>
2018-03-23 14:48:36 -0700

    stub out navigation

--- ScrumYourself/src/app/app.component.ts
+++ ScrumYourself/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute, Event, Router, NavigationEnd} from "@angular/router";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  private currentRoute: Router;
+  public path: string;
+
+  constructor(private route: Router) {
+    this.currentRoute = route;
+  }
+
+  ngOnInit() {
+    this.currentRoute.events.subscribe((event:Event) => {
+      if(event instanceof NavigationEnd ){
+        this.path = event.url;
+      }
+    });
+
+
+  }
+
 }
+
--- ScrumYourself/src/app/app.module.ts
+++ ScrumYourself/src/app/app.module.ts
@@ -2,19 +2,35 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {NgModule} from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
 
 
 import {AppComponent} from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
+const appRoutes: Routes = [
+  { path: '',
+    component: HomeComponent,
+    data: { title: 'My Tasks' },
+  },
+  { path: 'me',
+    component: ProfileComponent,
+    data: { title: 'Profile' }
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: false })
   ],
   providers: [],
   bootstrap: [AppComponent]
