947aa35240df67d52fc60ece93796c243c8f2899
Josh Pierro <josh_pierro@fsafood.com>
2018-03-23 12:37:11 -0700

    add material design framework and toolbar component

--- ScrumYourself/src/app/app.module.ts
+++ ScrumYourself/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 
 @NgModule({
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
